b068504f0b43f0a5783c6333c42c8d750374d766
RolfGutz <rolfgutz@hotmail.com>
2021-03-30 23:36:17 -0300

    Alteração de Menu Nav e inclusão do botão sair

--- app/__init__.py
+++ app/__init__.py
@@ -2,9 +2,12 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
+from flask_login import LoginManager, UserMixin, login_required,login_user, logout_user
+
 
 app = Flask(__name__)
 app.config.from_object('config')
+login_manager = LoginManager(app)
 
 db = SQLAlchemy(app)
 
--- app/controllers/default.py
+++ app/controllers/default.py
@@ -1,13 +1,51 @@
-from flask import render_template, request
-from app import app, db
+from flask import Flask,render_template,redirect,request,url_for,flash
+from app import app, db, login_manager
 from app.models.PessoaModel import Pessoa
 from app.models.UsuarioModel import UsuarioModel
+from flask_login import LoginManager, UserMixin, login_required,login_user, logout_user
 from werkzeug.security import generate_password_hash, check_password_hash
 
+
+
+# AUTENTICAÇÃO
+# *********************************************************
+
+@login_manager.user_loader
+def current_user(id):
+    return UsuarioModel.query.get(id)
+
 @app.route('/')
-@app.route('/login')
+@app.route('/login',methods =["GET","POST"])
 def login():
-    return render_template('login.html')
+	if request.method =="POST":
+		email = request.form['email']
+		password = request.form['password']
+		
+		usuario = UsuarioModel.query.filter_by(email=email).first()
+
+		if not usuario:
+			flash("Credênciais incorretas")
+			return redirect(url_for("login"))
+
+		if not check_password_hash(usuario.password, password):
+			flash("Senha  incorretas")
+			return redirect(url_for("login"))
+
+		login_user(usuario)
+		return redirect(url_for('listagem'))
+	
+	return render_template('login.html')
+
+
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return render_template("login.html")
+
+# *********************************************************
+
+
 
 # CADASTRO USUARIO
 # *********************************************************
@@ -41,6 +79,7 @@ def salvar_cadastro():
 
 
 @app.route('/listagem')
+@login_required
 def listagem():
 	pessoas = Pessoa.query.all()
 	return render_template('listagem.html', pessoas=pessoas, ordem='id')
