2a3353ac00a26284cdbdd238305a6cefb4a27d49
RolfGutz <rolfgutz@hotmail.com>
2021-06-11 19:41:41 -0300

    tratamento cnpj pedido

--- app/controllers/pedido/pedido_controller.py
+++ app/controllers/pedido/pedido_controller.py
@@ -268,7 +268,7 @@ def PedidoItensArray(pedido):
             produtosDoPedido.append(p)
             
     detalhesPedido = { 'razaoSocial' :  empresa.razao_social,
-                    'cnpj': FormatarCNPJ(empresa.cnpj),
+                    'cnpj': empresa.cnpj,
                     'numeroPedido' : pedido.id_pedido,
                     'DatadoPedido' : ConvertData(pedido.dataPedido),
                     'NomeUsuario':pedido.usuarioSistema.nome,
@@ -366,7 +366,3 @@ def ConverterPedidosAdm(pedido):
     
     return pedidoResult
 
-
-def FormatarCNPJ(cnpj):
-    cnpj = str(cnpj)
-    return "%s.%s.%s/%s-%s" % (cnpj[0:2],cnpj[2:5],cnpj[5:8],cnpj[8:12],cnpj[12:14])
